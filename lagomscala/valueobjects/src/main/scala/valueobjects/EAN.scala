7e24cda0f8b94d4bc1f4961d8ecef57f59dfa7b2
Dr. Stefan Reisner <stefan.reisner@springer.com>
2018-02-03 12:03:28 +0100

    updated EAN
    added format

--- lagomscala/valueobjects/src/main/scala/valueobjects/EAN.scala
+++ lagomscala/valueobjects/src/main/scala/valueobjects/EAN.scala
@@ -1,12 +1,16 @@
 package valueobjects
 
-import play.api.libs.json.{Format, Json}
+import play.api.libs.json._
 
 case class EAN(val underlying: String) extends AnyVal
 
 object EAN {
 
-  implicit val format: Format[EAN] = Json.format[EAN]
+  implicit object eanFormat extends Format[EAN] {
+    override def writes(o: EAN): JsValue = JsString(o.underlying)
+
+    override def reads(json: JsValue): JsResult[EAN] = JsSuccess(new EAN(json.as[String]))
+  }
 
 }
 
